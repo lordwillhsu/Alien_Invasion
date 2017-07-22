79f61e2bf4b3ba6e6150468a7a96d13d5fa638c6
Will Xu <lord.will.hsu@gmail.com>
2017-07-22 13:23:53 +0800

    Update to import ship attributes & game functions.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,29 +1,24 @@
 # Define Run_Game function of Alien Invasion
 # Created by Will Xu on July 16th, 2017
 
-import sys
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gfunc
 
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))  # Set window resolution;
-    pygame.display.set_caption("Alien Invasion")  # Set window caption;
-    ship = Ship(screen)  # Create object Ship;
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))     # Set window resolution;
+    pygame.display.set_caption("Alien Invasion")                                                # Set window caption;
+    ship = Ship(ai_settings, screen)                                                            # Create object Ship;
 
     while True:
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        pygame.display.flip()
-
+        gfunc.check_events(ship)
+        ship.update()
+        gfunc.update_screen(ai_settings, screen, ship)
 
 run_game()
