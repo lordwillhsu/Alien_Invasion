c9aa770aa6e7cd8c51b8edc1230c9d760eadce27
Will Xu <lord.will.hsu@gmail.com>
2017-07-22 13:16:30 +0800

    Add files via upload
    Defined ship object's movement flag and the movement control with Right/Left directions.

--- ship.py
+++ ship.py
@@ -1,20 +1,37 @@
 # Create game object Ship
-# Created by Will Xu on July 17th, 2017
+# Created by Will Xu on July 22nd, 2017
+
 
 import pygame
 
 
 class Ship:
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
+        """Initialize Ship object and set its initial position"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
-        self.image = pygame.image.load('images/ship.bmp')       # Load image for game object Ship
+        # Load ship image
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
+        # Set Ship's initial position to the center of the bottom
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
+
+        # Define ship movement status flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
